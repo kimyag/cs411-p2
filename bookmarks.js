13e0c6ffc8d41d029f62cd6d3945ba59470a36f3
Kimya Ghasemlou <kimya.ghasemloo@gmail.com>
2023-12-04 23:22:04 +0300

    page converted to popup

--- bookmarks.js
+++ bookmarks.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function () {
     displayBookmarks();
 });
 
-
-
 function displayBookmarks() {
     var bookmarksContainer = document.getElementById("bookmarks-container");
     var searchInput = document.getElementById("searchInput");
@@ -13,14 +11,34 @@ function displayBookmarks() {
         var bookmarks = result.bookmarks;
         var searchTerm = searchInput.value.toLowerCase();
 
-        bookmarks.forEach(function (bookmark) {
+        bookmarks.forEach(function (bookmark, index) {
             if (bookmark.title.toLowerCase().includes(searchTerm) || bookmark.url.toLowerCase().includes(searchTerm)) {
                 var bookmarkElement = document.createElement("div");
                 var linkElement = document.createElement("a");
+                var removeButton = document.createElement("button");
+                var dateElement = document.createElement("span");
+                linkElement.href = bookmark.url;
+
+
                 linkElement.href = bookmark.url;
                 linkElement.target = "_blank"; // Open link in a new tab
-                linkElement.textContent = `${bookmark.title} (Added on: ${bookmark.timestamp})`;
+                linkElement.textContent = bookmark.title;
+
+                dateElement.textContent = formatDate(bookmark.timestamp);
+
+                removeButton.innerHTML = "&#x2716;"; // Unicode for "x"
+                removeButton.className = "remove-button";
+                removeButton.addEventListener('click', function () {
+                    confirmRemoveBookmark(index, bookmark.title);
+                });
+                linkElement.style.display = "inline-block";
+                removeButton.style.display = "inline-block";
+                dateElement.style.display = "inline-block";
+
                 bookmarkElement.appendChild(linkElement);
+                bookmarkElement.appendChild(dateElement);
+                bookmarkElement.appendChild(removeButton);
+
                 bookmarksContainer.appendChild(bookmarkElement);
             }
         });
@@ -31,3 +49,26 @@ searchInput.addEventListener('input', function () {
     displayBookmarks();
 });
 
+function formatDate(timestamp) {
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return new Date(timestamp).toLocaleDateString('en-US', options);
+}
+
+function confirmRemoveBookmark(index, title) {
+    var confirmation = confirm(`Are you sure you want to remove the bookmark "${title}"?`);
+    if (confirmation) {
+        removeBookmark(index);
+    }
+}
+
+function removeBookmark(index) {
+    chrome.storage.local.get({ bookmarks: [] }, function (result) {
+        var bookmarks = result.bookmarks;
+        bookmarks.splice(index, 1);
+
+        chrome.storage.local.set({ bookmarks: bookmarks }, function () {
+            console.log("Bookmark removed successfully!");
+            displayBookmarks();
+        });
+    });
+}
--- popup.js
+++ popup.js
@@ -1,55 +1,116 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('addBookmarkButton').addEventListener('click', addBookmark);
-    document.getElementById('viewBookmarksButton').addEventListener('click', viewBookmarks);
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        var currentUrl = tabs[0].url;
+        var pageTitle = tabs[0].title;
+
+        document.getElementById("urlInput").value = currentUrl;
+        document.getElementById("titleInput").placeholder = pageTitle;
+    });
+
     displayBookmarks();
 });
 
-function addBookmark() {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var url = tabs[0].url;
-        if (url) {
-            saveBookmark(url);
-        } else {
-            console.error("Unable to get the current URL.");
-        }
-    });
-}
+document.getElementById("addBookmarkButton").addEventListener("click", function () {
+    addBookmark();
+});
 
-function saveBookmark(url) {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        var title = tabs[0].title || "Untitled";
+function addBookmark() {
+    var titleInput = document.getElementById("titleInput").value || document.getElementById("titleInput").placeholder;
+    var urlInput = document.getElementById("urlInput").value;
+
+    if (urlInput) {
         chrome.storage.local.get({ bookmarks: [] }, function (result) {
             var bookmarks = result.bookmarks;
-            bookmarks.push({ title: title, url: url, timestamp: new Date().toLocaleString() });
+
+            // Check if the URL already exists in bookmarks
+            var existingBookmarkIndex = bookmarks.findIndex(function (bookmark) {
+                return bookmark.url === urlInput;
+            });
+
+            if (existingBookmarkIndex !== -1) {
+                // Update the title of the existing bookmark
+                bookmarks[existingBookmarkIndex].title = titleInput;
+            } else {
+                // Add a new bookmark
+                var bookmark = {
+                    title: titleInput,
+                    url: urlInput,
+                    timestamp: new Date().getTime()
+                };
+                bookmarks.push(bookmark);
+            }
 
             chrome.storage.local.set({ bookmarks: bookmarks }, function () {
-                console.log("Bookmark added successfully!");
+                console.log("Bookmark added or updated successfully!");
                 displayBookmarks();
             });
         });
-    });
+    }
 }
 
-
-function viewBookmarks() {
-    chrome.tabs.create({ url: chrome.runtime.getURL('bookmarks.html') });
-}
 
 function displayBookmarks() {
     var bookmarksContainer = document.getElementById("bookmarks-container");
-    bookmarksContainer.innerHTML = "";
+    var searchInput = document.getElementById("searchInput");
+    bookmarksContainer.innerHTML = ""; // Clear previous bookmarks
 
     chrome.storage.local.get({ bookmarks: [] }, function (result) {
         var bookmarks = result.bookmarks;
-        bookmarks.forEach(function (bookmark) {
+
+        bookmarks.forEach(function (bookmark, index) {
             var bookmarkElement = document.createElement("div");
-            var linkElement = document.createElement("a");
-            linkElement.href = bookmark.url;
-            linkElement.target = "_blank"; // Open link in a new tab
-            linkElement.textContent = `${bookmark.title} (Added on: ${bookmark.timestamp})`;
-            bookmarkElement.appendChild(linkElement);
+            var titleElement = document.createElement("a");
+            var removeButton = document.createElement("button");
+            var dateElement = document.createElement("span");
+
+            bookmarkElement.className = "bookmark-item"; // Added a class for styling
+
+            titleElement.href = bookmark.url;
+            titleElement.target = "_blank"; // Open link in a new tab
+            titleElement.textContent = bookmark.title;
+
+            dateElement.textContent = formatDate(bookmark.timestamp);
+
+            removeButton.innerHTML = "&#x2716;"; // Unicode for "x"
+            removeButton.className = "remove-button";
+            removeButton.addEventListener('click', function () {
+                confirmRemoveBookmark(index, bookmark.title);
+            });
+
+            bookmarkElement.appendChild(titleElement);
+            bookmarkElement.appendChild(removeButton);
+            bookmarkElement.appendChild(dateElement);
+
             bookmarksContainer.appendChild(bookmarkElement);
         });
     });
 }
 
+function formatDate(timestamp) {
+    var options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return new Date(timestamp).toLocaleDateString('en-US', options);
+}
+
+function confirmRemoveBookmark(index, title) {
+    var confirmation = confirm(`Are you sure you want to remove the bookmark "${title}"?`);
+    if (confirmation) {
+        removeBookmark(index);
+    }
+}
+
+function removeBookmark(index) {
+    chrome.storage.local.get({ bookmarks: [] }, function (result) {
+        var bookmarks = result.bookmarks;
+        bookmarks.splice(index, 1);
+
+        chrome.storage.local.set({ bookmarks: bookmarks }, function () {
+            console.log("Bookmark removed successfully!");
+            displayBookmarks();
+        });
+    });
+}
+
+// Optional: If you want to include a search input for filtering bookmarks
+searchInput.addEventListener('input', function () {
+    displayBookmarks();
+});
